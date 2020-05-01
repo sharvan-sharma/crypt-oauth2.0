ce85ad9b7b2ab16dd9cf6c382022244f90b6d7bd
sharvan-sharma <sharvansharma518@gmail.com>
2020-05-01 19:55:43 +0530

    c-2

--- app.js
+++ app.js
@@ -78,5 +78,5 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-
-module.exports = app;
+app.listen(process.env.PORT || 5000,()=>console.log('server started'))
+//module.exports = app;
